3adb061bcc19daaf44d0678a7c846e006a09c147
Chance Yohman <chance.yohman@gmail.com>
2020-04-13 20:24:20 -0400

    Plotting curves Closes #6

--- c19generator.py
+++ c19generator.py
@@ -1,16 +1,19 @@
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #2020.04.11 cey
 #use filename provided by command- line
 
 c19_df =  pd.read_csv(sys.argv[1], sep=',')
-#print(c19_df.columns)
+print(c19_df.columns)
 
 uniqueStates = c19_df.state.unique()
 for state in uniqueStates:
 	state_df=c19_df[c19_df['state']==state]
 	print(state_df)
+	state_df.plot(kind='scatter',x='date',y='cases',color='red')
+	plt.show()
 
 #vt_df  = c19_df[c19_df['state']=='Vermont']
 #print(vt_df)
