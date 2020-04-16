08b3939d6daba7c0bd237b59cf6bc1264d04a498
Chance Yohman <chance.yohman@gmail.com>
2020-04-16 06:32:40 -0400

    Adjusted number of tick marks to 10. Relates to #10 .

--- c19generator.py
+++ c19generator.py
@@ -15,11 +15,10 @@ for state in uniqueStates:
 	state_df.plot(kind='scatter',x='date',y='cases',color='red')
 	plt.gcf().autofmt_xdate()
 	ax = plt.axes()
-	ax.xaxis.set_major_locator(plt.MaxNLocator(3))
+	ax.xaxis.set_major_locator(plt.MaxNLocator(10))
 	
 	print('Outputting '+state+' data')
 	plt.savefig(state+'.png')
-	#plt.show()
 
 #vt_df  = c19_df[c19_df['state']=='Vermont']
 #print(vt_df)
