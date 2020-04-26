61757a813b74e7976c1051d4162ef727b7f7c172
Chance Yohman <chance.yohman@gmail.com>
2020-04-26 08:27:55 -0400

    Closes #5

--- c19generator.py
+++ c19generator.py
@@ -1,12 +1,20 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+from datetime import date
 
 #2020.04.11 cey
 #use filename provided by command- line
 
 c19_df =  pd.read_csv(sys.argv[1], sep=',')
 print(c19_df.columns)
+
+path = os.getcwd()
+print ("The current working directory is %s" % path)
+savePath = path+"/"+date.today().strftime("%m-%d-%Y")+"/";
+os.mkdir(savePath);
+print ("The save directory is %s" % savePath)
 
 uniqueStates = c19_df.state.unique()
 for state in uniqueStates:
@@ -18,7 +26,7 @@ for state in uniqueStates:
 	ax.xaxis.set_major_locator(plt.MaxNLocator(20))
 	
 	print('Outputting '+state+' data')
-	plt.savefig(state+'.png')
+	plt.savefig(savePath+state+'.png')
 
 #vt_df  = c19_df[c19_df['state']=='Vermont']
 #print(vt_df)
