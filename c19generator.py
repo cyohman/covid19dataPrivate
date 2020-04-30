6a8de53dc7bc7e030aed9d38eecc65c5f620a648
Chance Yohman <chance.yohman@gmail.com>
2020-04-30 07:11:52 -0400

    Relates to #2

--- c19generator.py
+++ c19generator.py
@@ -21,27 +21,18 @@ print ("The save directory is %s" % savePath)
 uniqueStates = c19_df.state.unique()
 for state in uniqueStates:
 	state_df=c19_df[c19_df['state']==state]
+	
 	#print(state_df)
-	state_df.plot(kind='scatter',x='date',y='cases',color='red')
-	plt.gcf().autofmt_xdate()
-	ax = plt.axes()
-	ax.xaxis.set_major_locator(plt.MaxNLocator(20))
-
-        #data = covid_data[covid_data["Location"] == "Italy"]["Value"].values[::-1]
-	#agegroups = agegroup_lookup["Italy"]
-	#beds_per_100k = beds_lookup["Italy"]
-
-	#outbreak_shift = 30
-	# parameters to fit; form: {parameter: (initial guess, minimum value, max value)}
-	#params_init_min_max = {"R_0_start": (3.0, 2.0, 5.0), "k": (2.5, 0.01, 5.0), 
-        #               "x0": (90, 0, 120), "R_0_end": (0.9, 0.3, 3.5),
-        #               "prob_I_to_C": (0.05, 0.01, 0.1), "prob_C_to_D": (0.5, 0.05, 0.8),
-        #               "s": (0.003, 0.001, 0.01)}
-
+	
 	days = len(state_df['date'])
 	print(days)    
 	x_data = np.linspace(1, days, days, dtype=int)  # x_data i
 	print(x_data)
+	
+	#state_df.plot(kind='scatter',x=x_data,y='cases',color='red')
+	plt.gcf().autofmt_xdate()
+	ax = plt.axes()
+	ax.xaxis.set_major_locator(plt.MaxNLocator(20))
 
 	model = LognormalModel()
 	params = model.guess(state_df['cases'], x=x_data)
