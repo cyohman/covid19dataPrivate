b5300501af591db02c487a9e010e8de88df456e9
Chance Yohman <chance.yohman@gmail.com>
2020-04-30 19:14:28 -0400

    Relates to #2

--- c19generator.py
+++ c19generator.py
@@ -5,42 +5,48 @@ import os
 from datetime import date
 from lmfit.models import LognormalModel
 import numpy as np
+import scipy as sp
+from scipy.optimize import curve_fit
 
-#2020.04.11 cey
-#use filename provided by command- line
+#2020.04.30, cey, Need to figure out the right curve to fit here
+def func(x, a, b ):
+    return a + b*np.log(x)
+
+#2020.04.11, cey
+#use filename provided by command- line and values in those files are assumed to be separated by a comma
 
 c19_df =  pd.read_csv(sys.argv[1], sep=',')
 print(c19_df.columns)
 
 path = os.getcwd()
 print ("The current working directory is %s" % path)
+
+#2020.04.30, cey, Add the hour, minute, and second to the folder in 24 hour time
 savePath = path+"/"+date.today().strftime("%m-%d-%Y")+"/";
 os.mkdir(savePath);
 print ("The save directory is %s" % savePath)
 
+#2020.04.30, cey, The list of unique states & territories
 uniqueStates = c19_df.state.unique()
+
 for state in uniqueStates:
 	state_df=c19_df[c19_df['state']==state]
 	
-	#print(state_df)
-	
-	days = len(state_df['date'])
-	print(days)    
-	x_data = np.linspace(1, days, days, dtype=int)  # x_data i
-	print(x_data)
-	
-	#state_df.plot(kind='scatter',x=x_data,y='cases',color='red')
-	plt.gcf().autofmt_xdate()
-	ax = plt.axes()
-	ax.xaxis.set_major_locator(plt.MaxNLocator(20))
+	numDays = len(state_df['date'])
+	xData = np.linspace(1, numDays, numDays, dtype=int) 
+	#print(xData)
 
-	model = LognormalModel()
-	params = model.guess(state_df['cases'], x=x_data)
-	result = model.fit(state_df['cases'], params, x=x_data)   
-	result.plot_fit()
+	y= state_df['cases']
+
+	#2020.04.30, cey, Need to figure out what popt and pcov are
+	popt, pcov = curve_fit(func, xData, y)	
+
+	plt.plot(xData, y, 'ko', label="Original Case Data")	
+	plt.plot(xData, func(xData, *popt), 'r-', label="Fitted Curve")
 
 	print('Outputting '+state+' data')
 	plt.savefig(savePath+state+'.png')
+	
 	#2020.04.26, chance.yohman@gmail.com, Fix the 20 plots warning
 	plt.close()
 
